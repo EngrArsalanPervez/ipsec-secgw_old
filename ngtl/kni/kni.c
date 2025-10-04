de3d89626a40e300058c6bda0832b66cb3210edc
EngrArsalanPervez <usman.pervez10@gmail.com>
2025-10-05 03:01:40 +0500

    modification

--- ngtl/kni/kni.c
+++ ngtl/kni/kni.c
@@ -126,7 +126,7 @@ void kni_egress(struct kni_port_params *p) {
     // Only send IKE Traffic
 
     /* Burst tx to eth */
-    nb_tx = rte_eth_tx_burst(1, 1, pkts_burst, (uint16_t)num);
+    nb_tx = rte_eth_tx_burst(3, 1, pkts_burst, (uint16_t)num);
     if (nb_tx)
       kni_stats[port_id].tx_packets += nb_tx;
     if (unlikely(nb_tx < num)) {
