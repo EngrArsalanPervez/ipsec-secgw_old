f87ab1cb190c0c12b7ba6a3444bd8c83084b9c84
EngrArsalanPervez <usman.pervez10@gmail.com>
2025-10-05 02:28:28 +0500

    modification

--- ngtl/kni/kni.h
+++ ngtl/kni/kni.h
@@ -38,34 +38,7 @@ struct kni_interface_stats {
   uint64_t tx_dropped;
 };
 
-/* Globals (defined in kni.c) */
-extern struct rte_mempool *pktmbuf_pool;
-extern uint32_t ports_mask;
-extern int promiscuous_on_kni;
-extern volatile uint32_t kni_stop;
-extern volatile uint32_t kni_pause;
-extern int monitor_links;
 int kni_main(struct rte_mempool *shared_pool);
-int kni_free_kni(uint16_t port_id);
 void signal_handler_kni(void);
-void kni_burst_free_mbufs(struct rte_mbuf **pkts, unsigned num);
-void kni_ingress(struct kni_port_params *p);
-void kni_egress(struct kni_port_params *p);
-int main_loop(void *arg);
-void handle_kni_packet(struct rte_mbuf *buff);
-// static uint32_t parse_unsigned(const char *portmask);
-void print_config(void);
-int parse_config_kni(const char *arg);
-// int validate_parameters(uint32_t portmask);
-void init_kni(void);
-void init_port(uint16_t port);
 
-static int kni_change_mtu(uint16_t port_id, unsigned int new_mtu);
-int kni_config_network_interface(uint16_t port_id, uint8_t if_up);
-int kni_config_mac_address(uint16_t port_id, uint8_t mac_addr[]);
-
-void print_ethaddr1(const char *name, struct rte_ether_addr *mac_addr);
-int kni_alloc(uint16_t port_id);
-int kni_free_kni(uint16_t port_id);
-int kni_main_loop(__rte_unused void *arg);
 #endif /* _KNI_H_ */
