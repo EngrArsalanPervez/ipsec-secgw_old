7f50134446fe28b3bd6e828ec9046ec653cf4ff1
EngrArsalanPervez <usman.pervez10@gmail.com>
2025-10-05 21:55:16 +0500

    modification

--- config/dpdk_lclos.sh
+++ config/dpdk_lclos.sh
@@ -5,10 +5,10 @@ insmod ./build/kernel/linux/kni/rte_kni.ko carrier=on
 ./usertools/dpdk-devbind.py --bind=igb_uio enp2s0f0
 ./usertools/dpdk-devbind.py --bind=igb_uio enp2s0f1
 echo 1024 >/sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
-cd examples/ipsec-secgw/
-../build/ipsec-secgw -c 0xf -n 4 --socket-mem 1024,0 --vdev "crypto_openssl" -- -p 0x3 -P -u 0x1 --config="(0,0,0),(1,0,1)" -f ./IPE.cfg --transfer-mode poll -t 1
+cd examples/ipsec-gw/config
+../build/ipsec-secgw -c 0xf -n 4 --socket-mem 1024,0 --vdev "crypto_openssl" -- -p 0x3 -P -u 0x1 --config="(0,0,0),(1,0,1)" -f ./HCLOS.cfg --transfer-mode poll -t 1
 until ipsec-secgw; do
   echo "Server 'ipsec-secgw' crashed with exit code $?.  Respawning.." >&2
   sleep 2
-  ../build/ipsec-secgw -c 0xf -n 4 --socket-mem 1024,0 --vdev "crypto_openssl" -- -p 0x3 -P -u 0x1 --config="(0,0,0),(1,0,1)" -f ./IPE.cfg --transfer-mode poll -t 1
+  ../build/ipsec-secgw -c 0xf -n 4 --socket-mem 1024,0 --vdev "crypto_openssl" -- -p 0x3 -P -u 0x1 --config="(0,0,0),(1,0,1)" -f ./HCLOS.cfg --transfer-mode poll -t 1
 done
