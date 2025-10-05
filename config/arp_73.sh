fd619bd514149bcce74f5d389841035cf1c2cb71
EngrArsalanPervez <usman.pervez10@gmail.com>
2025-10-05 23:12:59 +0500

    modification

--- config/arp_73.sh
+++ config/arp_73.sh
@@ -1,3 +1,3 @@
 sudo ip link set dev vEth0_0 up
-ip addr add 20.20.20.2/24 dev vEth0_0
-arp -s 20.20.20.1 00:90:0b:d8:6e:aa
+ip addr add 30.30.30.2/24 dev vEth0_0
+arp -s 30.30.30.1 00:90:0b:d8:6e:aa
