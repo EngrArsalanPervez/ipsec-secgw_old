a0cf3c1d71ad0e594fa4060823ec278a06a3df9f
EngrArsalanPervez <usman.pervez10@gmail.com>
2025-10-05 21:46:23 +0500

    modification

---
+++ config/arp_74.sh
@@ -0,0 +1,3 @@
+sudo arp -s 192.168.5.1 00:90:0B:D8:6E:AA
+sudo arp -s 192.168.6.1 00:90:0B:D8:6E:AA
+sudo arp -s 192.168.7.1 00:90:0B:D8:6E:AA
---
+++ config/dpdk_lclos.sh
@@ -0,0 +1,14 @@
+cd /home/network/Desktop/dpdk
+modprobe uio
+insmod ./dpdk-kmods/linux/igb_uio/igb_uio.ko
+insmod ./build/kernel/linux/kni/rte_kni.ko carrier=on
+./usertools/dpdk-devbind.py --bind=igb_uio enp2s0f0
+./usertools/dpdk-devbind.py --bind=igb_uio enp2s0f1
+echo 1024 >/sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
+cd examples/ipsec-secgw/
+./build/ipsec-secgw -c 0xf -n 4 --socket-mem 1024,0 --vdev "crypto_openssl" -- -p 0x3 -P -u 0x1 --config="(0,0,0),(1,0,1)" -f ./IPE.cfg --transfer-mode poll -t 1
+until ipsec-secgw; do
+  echo "Server 'ipsec-secgw' crashed with exit code $?.  Respawning.." >&2
+  sleep 2
+  ./build/ipsec-secgw -c 0xf -n 4 --socket-mem 1024,0 --vdev "crypto_openssl" -- -p 0x3 -P -u 0x1 --config="(0,0,0),(1,0,1)" -f ./IPE.cfg --transfer-mode poll -t 1
+done
