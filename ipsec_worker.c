93cc5bd241590c1a5623134dd10675526bf4a377
EngrArsalanPervez <usman.pervez10@gmail.com>
2025-10-06 00:36:06 +0500

    modification

--- ipsec_worker.c
+++ ipsec_worker.c
@@ -943,13 +943,13 @@ static void ipsec_eventmode_worker(struct eh_conf *conf) {
 }
 
 int ipsec_launch_one_lcore(void *args) {
-  unsigned int nb_lcores = rte_lcore_count();
-  uint32_t lcore_id = rte_lcore_id();
-
-  if (lcore_id == (nb_lcores - 1) || lcore_id == (nb_lcores - 2)) {
-    kni_main(socket_ctx[0].mbuf_pool);
-    return 0;
-  }
+  /* unsigned int nb_lcores = rte_lcore_count(); */
+  /* uint32_t lcore_id = rte_lcore_id(); */
+  /**/
+  /* if (lcore_id == (nb_lcores - 1) || lcore_id == (nb_lcores - 2)) { */
+  /*   kni_main(socket_ctx[0].mbuf_pool); */
+  /*   return 0; */
+  /* } */
 
   struct eh_conf *conf;
 
