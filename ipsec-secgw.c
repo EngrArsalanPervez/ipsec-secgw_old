f3e07d02e34633a7d6886b7a7c6cdd56e2548d1b
EngrArsalanPervez <usman.pervez10@gmail.com>
2025-10-06 23:17:52 +0500

    modification

--- ipsec-secgw.c
+++ ipsec-secgw.c
@@ -470,11 +470,10 @@ static void print_stats_cb(__rte_unused void *param) {
     total_txRate += core_statistics[coreid].txRate;
 
     printf("\nStatistics for core %u ------------------------------"
-           "\nPackets received: %20" PRIu64 "\nPackets sent: %24" PRIu64
-           "\nPackets dropped: %21" PRIu64 "\nBurst percent: %23.2f"
+           "\nPackets received: %30" PRIu64 "\nPackets sent: %34" PRIu64
+           "\nPackets dropped: %31" PRIu64 "\nBurst percent: %23.2f"
            "\nPackets per Rx call: %17.2f"
            "\nPackets per Tx call: %17.2f"
-           "\n"
            "\nrx_bytes:\t%34" PRIu64 "\ntx_bytes:\t%34" PRIu64
            "\nrxBytesNormalized:\t%23s"
            "\ntxBytesNormalized:\t%23s"
